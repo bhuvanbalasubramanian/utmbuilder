1c2b17293bb2f99004cefc9dd6937751fb9d9be3
Bhuvaneswaran Balasubramanian <bhuvanesad@gmail.com>
2021-06-07 02:33:03 +0530

    fixed minor issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="mb-5 pt-1 text-muted text-center text-small">
       <p className="mb-1">
         © 2021 Developed by{" "}
-        <a href="https://www.bhuvaneswaran.com" target='_blank'>Bhuvaneswaran Balasubramanian</a>
+        <a href="https://www.bhuvaneswaran.com" target='_blank' rel="noreferrer">Bhuvaneswaran Balasubramanian</a>
       </p>
     </footer>
   );
